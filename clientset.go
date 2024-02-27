e520984ca772fdd131ef4c1880c6514c6708a7ce
JanLam <honos628@foxmail.com>
2024-02-27 23:34:17 +0800

    init & create k8s clientset

---
+++ clientset.go
@@ -0,0 +1,99 @@
+package k8sutils
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"k8s.io/apimachinery/pkg/version"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type Clientset struct {
+	clientset *kubernetes.Clientset
+}
+
+var (
+	cli              *Clientset
+	once             sync.Once
+	serverVersion    *version.Info
+	currentNamespace string
+)
+
+func NewClientSet() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		var configPath string
+		if p := os.Getenv(clientcmd.RecommendedConfigPathEnvVar); len(p) > 0 {
+			configPath = p
+		} else {
+			p = clientcmd.RecommendedHomeFile
+		}
+		config, err = clientcmd.BuildConfigFromFlags("", configPath)
+	}
+
+	if err != nil {
+		err = fmt.Errorf("error building kubeconfig: %w", err)
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
+}
+
+func GetClientset() (*Clientset, error) {
+	once.Do(func() {
+		var (
+			err       error
+			clientset *kubernetes.Clientset
+		)
+		cli = &Clientset{}
+
+		clientset, err = NewClientSet()
+		if err != nil {
+			err = fmt.Errorf("error creating Kubernetes client: %w", err)
+			return
+		}
+
+		cli.clientset = clientset
+
+		serverVersion, err = cli.clientset.Discovery().ServerVersion()
+		if err != nil {
+			err = fmt.Errorf("error getting server version: %w", err)
+			return
+		}
+
+		// try to read current namespace
+		if b, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err != nil {
+			currentNamespace = "default" // if error , set currentNamespace -> default
+			fmt.Println("default namespace")
+		} else {
+			currentNamespace = string(b)
+		}
+	})
+
+	return cli, nil
+}
+
+func (kc *Clientset) GetServerVersion() (string, error) {
+	if serverVersion != nil {
+		v := serverVersion
+		_ = v
+		return serverVersion.String(), nil
+	}
+
+	v, err := kc.clientset.Discovery().ServerVersion()
+	if err != nil {
+		return "", err
+	}
+	serverVersion = v
+	return serverVersion.String(), nil
+}
+
+func (kc *Clientset) GetNamespace() string {
+	return currentNamespace
+}
+
+func (kc *Clientset) GetClientSet() *kubernetes.Clientset {
+	return kc.clientset
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/linlanniao/k8sutils
+
+go 1.22.0
+
+require k8s.io/client-go v0.29.2 // indirect
