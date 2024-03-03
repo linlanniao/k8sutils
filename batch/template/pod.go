71ad9d8e47a959644dd83b6f80c92674d7945750
JanLam <honos628@foxmail.com>
2024-03-03 18:15:17 +0800

    dev: pod template

--- batch/template/pod.go
+++ batch/template/pod.go
@@ -2,6 +2,7 @@ package template
 
 import (
 	"fmt"
+	"path/filepath"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -21,7 +22,7 @@ const (
 	podRequestMemory         = "100Mi"
 	podLimitCPU              = "2000m"
 	podLimitMemory           = "2000Mi"
-	podRunnerContainerImage  = "busybox:1.28.4"
+	//podRunnerContainerImage  = "busybox:1.28.4"
 
 	podContainerNormalName  = "runner"
 	podContainerNsenterName = "runner-nsenter"
@@ -39,39 +40,80 @@ func (s scriptExecutor) String() string {
 	return string(s)
 }
 
-var (
-	podNsenterSecurityContextPrivileged = true
-)
+type PodTemplate struct {
+	pod              *corev1.Pod
+	namespace        string
+	name             string
+	namePrefix       string
+	image            string
+	isPrivileged     bool
+	scriptExecutor   scriptExecutor
+	scriptConfigMap  *corev1.ConfigMap // script content config map
+	configMapDataKey string            // key for configmap.data field
+	args             []string
+}
 
-func podBaseLabels(extraLabels ...map[string]string) map[string]string {
-	l := map[string]string{
-		podBaseLabelKey: podBaseLabelVal,
+func (p *PodTemplate) initPod() *PodTemplate {
+	// skip if pod is already initialized
+	if p.pod != nil {
+		return p
 	}
-	for _, el := range extraLabels {
-		for k, v := range el {
-			if k != podBaseLabelKey && v != podBaseLabelVal {
-				l[k] = v
-			}
+
+	p.pod = &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:         p.name,
+			Namespace:    p.namespace,
+			GenerateName: p.namePrefix,
+		},
+	}
+
+	// init pod spec
+	p.pod.Spec = corev1.PodSpec{
+		Volumes:            nil,
+		Containers:         nil,
+		RestartPolicy:      corev1.RestartPolicyNever,
+		NodeSelector:       nil,
+		ServiceAccountName: "",
+		NodeName:           "",
+		HostNetwork:        false,
+		Affinity:           nil,
+	}
+	if p.isPrivileged {
+		p.pod.Spec.HostNetwork = true
+		p.pod.Spec.HostPID = true
+	}
+
+	// init volumes
+	if !p.isPrivileged {
+		// The pod with ordinary permission needs to mount the command of the host kubectl into the container.
+		p.pod.Spec.Volumes = []corev1.Volume{
+			{
+				Name: podSharedVolumeName,
+				VolumeSource: corev1.VolumeSource{
+					EmptyDir: &corev1.EmptyDirVolumeSource{},
+				},
+			},
+			{
+				Name: "kubectl1",
+				VolumeSource: corev1.VolumeSource{
+					HostPath: &corev1.HostPathVolumeSource{Path: "/usr/bin/kubectl"},
+				},
+			},
+			{
+				Name: "kubectl2",
+				VolumeSource: corev1.VolumeSource{
+					HostPath: &corev1.HostPathVolumeSource{Path: "/usr/local/bin/kubectl"},
+				},
+			},
 		}
 	}
-	return l
-}
 
-func podNsenterContainerCommand() []string {
-	return []string{
-		"nsenter", "--target", "1", "--mount", "--uts", "--ipc", "--net", "--pid",
-		//"sh", "-c", `echo runner`,
-	}
-}
-
-func newPodContainers(isNsenter bool, executor scriptExecutor) (newerContainers []corev1.Container) {
-	newerContainers = make([]corev1.Container, 1)
-
-	// set containers
-	if !isNsenter {
-		newerContainers[0] = corev1.Container{
-			Name:       "runner",
-			Image:      podRunnerContainerImage,
+	// init containers
+	p.pod.Spec.Containers = make([]corev1.Container, 1)
+	if !p.isPrivileged {
+		p.pod.Spec.Containers[0] = corev1.Container{
+			Name:       podContainerNormalName,
+			Image:      p.image,
 			Command:    nil,
 			Args:       nil,
 			WorkingDir: podWorkDir,
@@ -117,9 +159,9 @@ func newPodContainers(isNsenter bool, executor scriptExecutor) (newerContainers 
 		}
 	} else {
 		podNsenterSecurityContextPrivileged := true
-		newerContainers[0] = corev1.Container{
+		p.pod.Spec.Containers[0] = corev1.Container{
 			Name:    podContainerNsenterName,
-			Image:   podRunnerContainerImage,
+			Image:   p.image,
 			Command: nil,
 			Args:    nil,
 			Env: []corev1.EnvVar{
@@ -148,160 +190,24 @@ func newPodContainers(isNsenter bool, executor scriptExecutor) (newerContainers 
 		}
 	}
 
-	// set script executor
-	if !isNsenter {
-		switch executor {
+	// init script executor
+	if !p.isPrivileged {
+		switch e := p.scriptExecutor; e {
 		case scriptExecutorBash:
-			newerContainers[0].Command = []string{executor.String(), "-c"}
+			p.pod.Spec.Containers[0].Command = []string{e.String(), "-l"}
 		case scriptExecutorPython:
-			newerContainers[0].Command = []string{executor.String()}
+			p.pod.Spec.Containers[0].Command = []string{e.String()}
 		}
-
+		p.pod.Spec.Containers[0].Command = []string{p.scriptExecutor.String()}
 	} else {
-		newerContainers[0].Command = []string{
+		p.pod.Spec.Containers[0].Command = []string{
 			"nsenter", "--target", "1", "--mount", "--uts", "--ipc", "--net", "--pid",
-			"bash", "-c",
-			//"sh", "-c", `echo runner`,
+			scriptExecutorBash.String(), "-l",
 		}
 	}
 
-	return
-}
+	return p
 
-// Template for normal's pod
-var podTemplateNormal = corev1.Pod{
-	ObjectMeta: metav1.ObjectMeta{
-		Namespace: podDefaultNamespace,
-		Labels:    podBaseLabels(),
-	},
-	Spec: corev1.PodSpec{
-		Volumes: []corev1.Volume{
-			{
-				Name: podSharedVolumeName,
-				VolumeSource: corev1.VolumeSource{
-					EmptyDir: &corev1.EmptyDirVolumeSource{},
-				},
-			},
-			{
-				Name: "kubectl1",
-				VolumeSource: corev1.VolumeSource{
-					HostPath: &corev1.HostPathVolumeSource{Path: "/usr/bin/kubectl"},
-				},
-			},
-			{
-				Name: "kubectl2",
-				VolumeSource: corev1.VolumeSource{
-					HostPath: &corev1.HostPathVolumeSource{Path: "/usr/local/bin/kubectl"},
-				},
-			},
-		},
-		Containers: []corev1.Container{
-			{
-				Name:       "runner",
-				Image:      podRunnerContainerImage,
-				Args:       nil,
-				WorkingDir: podWorkDir,
-				Env: []corev1.EnvVar{
-					{
-						Name:  "PATH",
-						Value: "/hostroot/usr/local/bin:/hostroot/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-					},
-					{
-						Name:  "TERM",
-						Value: "dumb",
-					},
-				},
-				Resources: corev1.ResourceRequirements{
-					Limits: corev1.ResourceList{
-						corev1.ResourceCPU:    resource.MustParse(podLimitCPU),
-						corev1.ResourceMemory: resource.MustParse(podLimitMemory),
-					},
-					Requests: corev1.ResourceList{
-						corev1.ResourceCPU:    resource.MustParse(podRequestCPU),
-						corev1.ResourceMemory: resource.MustParse(podRequestMemory),
-					},
-				},
-
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      podSharedVolumeName,
-						MountPath: podSharedVolumeMountPath,
-					},
-					{
-						Name:      "kubectl1",
-						ReadOnly:  true,
-						MountPath: "/hostroot/usr/bin/kubectl",
-						SubPath:   "",
-					},
-					{
-						Name:      "kubectl2",
-						ReadOnly:  true,
-						MountPath: "/hostroot/usr/local/bin/kubectl",
-					},
-				},
-				ImagePullPolicy: corev1.PullIfNotPresent,
-			},
-		},
-		RestartPolicy:      corev1.RestartPolicyNever,
-		NodeSelector:       nil,
-		ServiceAccountName: "",
-		NodeName:           "",
-		HostNetwork:        false,
-		Affinity:           nil,
-	},
-}
-
-// Template for nsenter's pod
-var podTemplateNsenter = corev1.Pod{
-	Spec: corev1.PodSpec{
-		Containers: []corev1.Container{
-			{
-				Name:    "runner",
-				Image:   podRunnerContainerImage,
-				Command: podNsenterContainerCommand(),
-				Env: []corev1.EnvVar{
-					{
-						Name:  "TERM",
-						Value: "dumb",
-					},
-				},
-				Resources: corev1.ResourceRequirements{
-					Limits: corev1.ResourceList{
-						corev1.ResourceCPU:    resource.MustParse(podLimitCPU),
-						corev1.ResourceMemory: resource.MustParse(podLimitMemory),
-					},
-					Requests: corev1.ResourceList{
-						corev1.ResourceCPU:    resource.MustParse(podRequestCPU),
-						corev1.ResourceMemory: resource.MustParse(podRequestMemory),
-					},
-				},
-				ImagePullPolicy: corev1.PullIfNotPresent,
-				SecurityContext: &corev1.SecurityContext{
-					Privileged: &podNsenterSecurityContextPrivileged,
-				},
-				Stdin:     true,
-				StdinOnce: true,
-				TTY:       true,
-			},
-		},
-
-		RestartPolicy:      corev1.RestartPolicyNever,
-		NodeSelector:       nil,
-		ServiceAccountName: "",
-		NodeName:           "",
-		HostNetwork:        true,
-		HostPID:            true,
-		Affinity:           nil,
-	},
-}
-
-type PodTemplate struct {
-	pod            *corev1.Pod
-	namespace      string
-	name           string
-	isPrivileged   bool
-	namePrefix     string
-	scriptExecutor scriptExecutor
 }
 
 func (p *PodTemplate) Pod() *corev1.Pod {
@@ -315,10 +221,17 @@ func (p *PodTemplate) SetDefaultNamespaceIfEmpty() *PodTemplate {
 	return p
 }
 
-func (p *PodTemplate) SetScriptVolume(scriptContentConfigMap *corev1.ConfigMap) *PodTemplate {
+func (p *PodTemplate) SetScriptMount(scriptConfigMap *corev1.ConfigMap, dataKey string) *PodTemplate {
+	p.initPod()
+
 	if p.pod.Spec.Volumes == nil {
 		p.pod.Spec.Volumes = []corev1.Volume{}
 	}
+
+	// upsert to PodTemplate
+	p.scriptConfigMap = scriptConfigMap
+	p.configMapDataKey = dataKey
+
 	const volumeName = "script-volume"
 
 	// If the volume already exists, skip it.
@@ -329,18 +242,21 @@ func (p *PodTemplate) SetScriptVolume(scriptContentConfigMap *corev1.ConfigMap) 
 	}
 
 	// Add the script volume.
-	optional := false
-	scriptName := fmt.Sprintf("%s-%s", "scriptcontent", p.name)
+	optional := false // configmap must exist
+
+	// Nsenter mount files with pod names to avoid duplicate file names
+	scriptName := fmt.Sprintf("%s-%s", "script", p.name)
+
 	scriptVolume := corev1.Volume{
 		Name: volumeName,
 		VolumeSource: corev1.VolumeSource{
 			ConfigMap: &corev1.ConfigMapVolumeSource{
 				LocalObjectReference: corev1.LocalObjectReference{
-					Name: "",
+					Name: p.scriptConfigMap.GetName(),
 				},
 				Items: []corev1.KeyToPath{
 					{
-						Key:  "optional",
+						Key:  p.configMapDataKey,
 						Path: scriptName,
 					},
 				},
@@ -350,26 +266,44 @@ func (p *PodTemplate) SetScriptVolume(scriptContentConfigMap *corev1.ConfigMap) 
 	}
 	p.pod.Spec.Volumes = append(p.pod.Spec.Volumes, scriptVolume)
 
+	// find runner container
+	var runnerContainer *corev1.Container
+	for _, c := range p.pod.Spec.Containers {
+		if c.Name == podContainerNormalName || c.Name == podContainerNsenterName {
+			runnerContainer = &c
+			break
+		}
+	}
+	if runnerContainer == nil {
+		// if runner container not found, return
+		return p
+	}
+
 	// Add the script volume mount.
-	if p.pod.Spec.Containers == nil {
-		p.pod.Spec.Containers = newPodContainers(p.isPrivileged, p.scriptExecutor)
+	if runnerContainer.VolumeMounts == nil {
+		runnerContainer.VolumeMounts = []corev1.VolumeMount{}
 	}
-	if p.pod.Spec.Containers[0].VolumeMounts == nil {
-		p.pod.Spec.Containers[0].VolumeMounts = []corev1.VolumeMount{}
-	}
-	p.pod.Spec.Containers[0].VolumeMounts = append(p.pod.Spec.Containers[0].VolumeMounts, corev1.VolumeMount{
+	runnerContainer.VolumeMounts = append(runnerContainer.VolumeMounts, corev1.VolumeMount{
 		Name:      volumeName,
 		ReadOnly:  true,
 		MountPath: scriptContentMountPath,
 	})
 
+	// setup args
+	if runnerContainer.Args == nil {
+		p.pod.Spec.Containers[0].Args = []string{}
+	}
+	// add script path
+	scriptFullPath := filepath.Join(scriptContentMountPath, scriptName)
+	runnerContainer.Args = append(runnerContainer.Args, scriptFullPath)
+
+	// add extra args
+	runnerContainer.Args = append(runnerContainer.Args, p.args...)
+
 	return p
 }
 
-func (p *PodTemplate) InitRunnerContainer() {
-
-}
-
+// TODO
 func NewPodTemplate(
 	namespace string,
 	name string,
@@ -379,12 +313,8 @@ func NewPodTemplate(
 	image string,
 
 ) *PodTemplate {
-	var p *corev1.Pod
-	if !isPrivileged {
-		p = podTemplateNormal.DeepCopy()
-	} else {
-		p = podTemplateNsenter.DeepCopy()
-	}
+	tmpl := new(PodTemplate)
+	tmpl.initPod()
 
 	p.SetName(name)
 	p.SetNamespace(namespace)
